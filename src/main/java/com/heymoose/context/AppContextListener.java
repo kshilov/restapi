43b28c36dec6a0b4396279317ab28ba5d4abe760
Denis Borovikov <denis.borovikov@gmail.com>
2011-09-25 22:25:19 +0400

    prod mod by default

--- src/main/java/com/heymoose/context/AppContextListener.java
+++ src/main/java/com/heymoose/context/AppContextListener.java
@@ -2,6 +2,7 @@ package com.heymoose.context;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Stage;
 import com.google.inject.servlet.GuiceServletContextListener;
 import com.heymoose.security.SecurityModule;
 
@@ -9,6 +10,7 @@ public class AppContextListener extends GuiceServletContextListener {
   @Override
   protected Injector getInjector() {
     return Guice.createInjector(
+            Stage.PRODUCTION,
             new SettingsModule(),
             new JerseyModule(),
             new CommonModule(),
