e152a771a1965899626b17dc542bbe3040a4970a
Eugeny Slezko <mratozar@gmail.com>
2011-11-15 15:27:32 +0300

    XmlPerformer

---
+++ src/main/java/com/heymoose/resource/xml/XmlPerformer.java
@@ -0,0 +1,28 @@
+package com.heymoose.resource.xml;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "performer")
+public class XmlPerformer {
+  @XmlAttribute(name = "id")
+  public Long id;
+  
+  @XmlAttribute(name = "ext-id")
+  public String extId;
+  
+  @XmlAttribute(name = "app")
+  public XmlApp app;
+  
+  @XmlAttribute(name = "creation-time")
+  public String creationTime;
+  
+  @XmlAttribute(name = "inviter")
+  public XmlPerformer inviter;
+  
+  @XmlAttribute(name = "male")
+  public Boolean male;
+  
+  @XmlAttribute(name = "age")
+  public Integer age;
+}
