bf537aafa34ba519813de8a5150097f40ce7394f
Denis Borovikov <d.borovikov@hh.ru>
2011-12-06 00:04:23 +0400

    increased concurrency level

--- src/main/java/com/heymoose/server/Launcher.java
+++ src/main/java/com/heymoose/server/Launcher.java
@@ -35,7 +35,7 @@ public class Launcher {
     if (server != null)
       return;
     server = new GrizzlyWebServer(port);
-    int nThreads = Runtime.getRuntime().availableProcessors() * 8;
+    int nThreads = Runtime.getRuntime().availableProcessors() * 64;
     server.setCoreThreads(nThreads);
     server.setMaxThreads(nThreads);
 		ServletAdapter adapter = new ServletAdapter(new StubServlet());
