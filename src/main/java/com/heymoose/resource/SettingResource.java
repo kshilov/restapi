cb7db76c1d3c1aea05e214133d5746f848dff01c
Eugeny Slezko <mratozar@gmail.com>
2012-02-15 15:17:18 +0400

    Settings update fixed

--- src/main/java/com/heymoose/resource/SettingResource.java
+++ src/main/java/com/heymoose/resource/SettingResource.java
@@ -5,17 +5,15 @@ import java.util.Map;
 import com.heymoose.domain.settings.Settings;
 import com.heymoose.hibernate.Transactional;
 import com.heymoose.resource.xml.XmlSettings;
-import com.sun.jersey.api.core.HttpContext;
-import com.sun.jersey.api.representation.Form;
 
 import static com.heymoose.util.WebAppUtil.checkNotNull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
 
 @Singleton
 @Path("settings")
@@ -49,10 +47,10 @@ public class SettingResource {
   
   @PUT
   @Transactional
-  public void update(@Context HttpContext context) {
-    Form params = context.getRequest().getEntity(Form.class);
-    for (String name : params.keySet()) {
-      settings.set(name, params.getFirst(name));
-    }
+  public void update(@FormParam("m") Double m, @FormParam("q") Double q) {
+    if (m != null)
+      settings.set(Settings.M, m);
+    if (q != null)
+      settings.set(Settings.Q, q);
   }
 }
