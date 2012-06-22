c8166176789d0dc3d68755f9cf9645989598bede
filipovskii_off <filipovskii.off@gmail.com>
2012-06-22 20:24:53 +0400

    Filter by dates in ErrorInfo resource.

--- src/main/java/com/heymoose/resource/ErrorInfoResource.java
+++ src/main/java/com/heymoose/resource/ErrorInfoResource.java
@@ -1,8 +1,12 @@
 package com.heymoose.resource;
 
+import com.heymoose.domain.affiliate.ErrorInfo;
 import com.heymoose.domain.affiliate.ErrorInfoRepository;
 import com.heymoose.resource.xml.Mappers;
 import com.heymoose.resource.xml.XmlErrorsInfo;
+import org.joda.time.DateTime;
+
+import java.util.List;
 
 public final class ErrorInfoResource {
 
@@ -12,7 +16,11 @@ public final class ErrorInfoResource {
     this.repository = repository;
   }
 
-  public XmlErrorsInfo list(int offset, int limit, Long affId) {
-    return Mappers.toXmlErrorsInfo(repository.list(offset, limit, affId));
+  public XmlErrorsInfo list(int offset, int limit, Long affId,
+                            Long startTime, Long endTime) {
+    DateTime dateFrom = new DateTime(startTime);
+    DateTime dateTo = new DateTime(endTime);
+    List<ErrorInfo> result = repository.list(offset, limit, affId, dateFrom, dateTo);
+    return Mappers.toXmlErrorsInfo(result);
   }
 }
