386f3f2f7846c2fa8191bb2f459083cdb28cedde
filipovskii_off <filipovskii.off@gmail.com>
2012-06-25 19:59:24 +0400

    SQL for getting overall statistics out.

--- src/main/java/com/heymoose/domain/affiliate/OfferStats.java
+++ src/main/java/com/heymoose/domain/affiliate/OfferStats.java
@@ -499,6 +499,25 @@ public class OfferStats {
     return new Pair<List<OverallOfferStats>, Long>(stats, count);
   }
 
+  public Object overallStats(DateTime from, DateTime to) {
+    String sql =
+        "select " +
+          "sum(canceled_revenue)                                 canceled, " +
+          "sum(not_confirmed_revenue)                            not_confirmed_partner, " +
+          "sum(not_confirmed_revenue * (p_aff.fee / 100.0))      not_confirmed_fee, " +
+          "sum(not_confirmed_revenue * (1 + p_aff.fee / 100.0))  not_confirmed_sum, " +
+          "sum(confirmed_revenue)                                confirmed_partner, " +
+          "sum(confirmed_revenue * (p_aff.fee / 100.0))          confirmed_fee, " +
+          "sum(confirmed_revenue * (1 + p_aff.fee / 100.0))      confirmed_sum " +
+        "from " +
+          "offer_stat " +
+        "left join " +
+          "user_profile p_aff on offer_stat.aff_id = p_aff.id " +
+        "where " +
+          "creation_time between :from and :to";
+    return null;
+  }
+
   private static Long extractLong(Object val) {
     if (val == null)
       return 0L;
