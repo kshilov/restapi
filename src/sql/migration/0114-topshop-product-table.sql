bde4cf230f33575c8566661bbe29963fc464e146
filipovskii_off <filipovskii.off@gmail.com>
2012-07-23 13:52:19 +0400

    TopShopProduct entity removed. We are using suboffers instead.

--- src/sql/migration/0114-topshop-product-table.sql
+++
@@ -1,8 +0,0 @@
-create table topshop_product (
-  id bigint not null,
-  offer_id bigint references offer(id),
-  price numeric(19,2) not null
-);
-
-alter table topshop_product
-  add constraint topshop_product_pk primary key(id);
