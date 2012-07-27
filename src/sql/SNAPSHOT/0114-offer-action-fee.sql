51e7175c83573a36f13b82ffee9ab1e4bb2c9118
filipovskii_off <filipovskii.off@gmail.com>
2012-07-27 18:19:51 +0400

    Migration.

--- src/sql/SNAPSHOT/0114-offer-action-fee.sql
+++ src/sql/SNAPSHOT/0114-offer-action-fee.sql
@@ -17,6 +17,86 @@ create function offer_stat_fee_approve(stat_id bigint, amount numeric) returns b
     returning id;
   $_$;
 
+create function offer_stat_fee_cancel(stat_id bigint, amount numeric)
+returns bigint
+language sql
+as $_$
+
+  update offer_stat
+  set canceled_fee = coalesce(canceled_fee, 0.0) + $2,
+  not_confirmed_fee = coalesce(not_confirmed_fee, 0.0) - $2
+  where id = $1
+  returning id;
+
+$_$;
+
+create function offer_stat_fee_create(stat_id bigint, amount numeric)
+returns bigint
+language sql
+as $_$
+
+  update offer_stat
+  set not_confirmed_fee = coalesce(not_confirmed_fee, 0.0) + $2
+  where id = $1
+  returning id;
+
+$_$;
+
+/* fill in the data */
+
+/* not_confirmed_fee */
+
+select offer_stat_fee_create(action.stat_id, entry.amount)
+from accounting_entry entry
+
+left join offer_action action
+on action.id = entry.source_id
+
+left join admin_account_not_confirmed admin
+on admin.id is not null
+
+where
+  entry.event = 1 /* action created */
+  and entry.amount > 0
+  and entry.account_id = admin.account_id;
+
+/* canceled fee */
+
+select offer_stat_fee_cancel(action.stat_id, -entry.amount)
+from accounting_entry entry
+
+left join offer_action action
+on action.id = entry.source_id
+
+left join admin_account_not_confirmed admin
+on admin.id is not null
+
+where
+  entry.event = 3 /* action canceled */
+  and entry.amount < 0
+  and entry.account_id = admin.account_id;
+
+/* confirmed_fee */
+
+select offer_stat_fee_approve(action.stat_id, entry.amount)
+from accounting_entry entry
+
+left join offer_action action
+on action.id = entry.source_id
+
+left join admin_account_not_confirmed admin
+on admin.id is not null
+
+left join accounting_entry canceled
+on canceled.source_id = entry.id
+and canceled.event = 8
+
+where
+  entry.event = 2 /* action approved */
+  and canceled.id is null
+  and entry.account_id = admin.account_id
+  and entry.amount < 0;
+
 drop function approve(bigint);
 
 CREATE FUNCTION approve(action_id bigint) RETURNS bigint
