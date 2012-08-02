2fb2059cd0910c8bec412b6b97b7c09aa21500f8
filipovskii_off <filipovskii.off@gmail.com>
2012-08-02 16:34:41 +0400

    Delikateska YML importer.

--- src/main/java/com/heymoose/infrastructure/service/delikateska/DelikateskaYmlImporter.java
+++ src/main/java/com/heymoose/infrastructure/service/delikateska/DelikateskaYmlImporter.java
@@ -1,5 +1,34 @@
 package com.heymoose.infrastructure.service.delikateska;
 
-public final class DelikateskaYmlImporter {
+import com.heymoose.domain.base.Repo;
+import com.heymoose.infrastructure.service.yml.Offer;
+import com.heymoose.infrastructure.service.yml.YmlCatalog;
+import com.heymoose.infrastructure.service.yml.YmlImporter;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
+public final class DelikateskaYmlImporter extends YmlImporter {
+
+  private static final BigDecimal DEFAULT_PERCENT = new BigDecimal(10);
+
+  private final Map<Integer, Integer> idPercentMap;
+
+  public DelikateskaYmlImporter(Repo repo, Map<Integer, Integer> idPercentMap) {
+    super(repo);
+    this.idPercentMap = idPercentMap;
+  }
+
+  @Override
+  protected BigDecimal getPercent(Offer catalogOffer, YmlCatalog catalog) {
+    Integer offerId = Integer.valueOf(catalogOffer.getId());
+    if (idPercentMap.containsKey(offerId))
+      return new BigDecimal(idPercentMap.get(offerId));
+    return DEFAULT_PERCENT;
+  }
+
+  @Override
+  protected boolean isExclusive(Offer catalogOffer, YmlCatalog catalog) {
+    return idPercentMap.containsKey(Integer.valueOf(catalogOffer.getId()));
+  }
 }
