2decc7b24c43aa86903100868cf1162c9d679399
filipovskii_off <filipovskii.off@gmail.com>
2012-10-11 14:01:43 +0400

    Primary and foreign keys.

--- src/sql/SNAPSHOT/0183-product-table.sql
+++ src/sql/SNAPSHOT/0183-product-table.sql
@@ -7,6 +7,29 @@ drop sequence if exists product_seq;
 drop sequence if exists shop_category_seq;
 drop sequence if exists product_attribute_seq;
 
+create sequence shop_category_seq
+    start with 1
+    increment by 1
+    no minvalue
+    no maxvalue
+    cache 1;
+
+create table shop_category(
+  id bigint default nextval('shop_category_seq'),
+  offer_id bigint not null,
+  original_id varchar(32) not null,
+  name varchar(255) not null,
+  parent_id bigint);
+
+alter table shop_category add constraint shop_category_pk
+primary key (id);
+
+alter table shop_category add constraint shop_category_offer_fk
+foreign key (offer_id) references offer(id) on update cascade;
+
+alter table shop_category add constraint shop_category_parent_fk
+foreign key (parent_id) references shop_category(id) on update cascade;
+
 create sequence product_seq
     start with 1
     increment by 1
@@ -25,6 +48,16 @@ create table product(
   creation_time timestamp without time zone default now() not null,
   last_change_time timestamp without time zone default now() not null);
 
+alter table product add constraint product_pk
+primary key (id);
+
+alter table product add constraint product_offer_fk
+foreign key (offer_id) references offer(id) on update cascade;
+
+alter table product add constraint product_shop_category_fk
+foreign key (shop_category_id) references shop_category(id)
+on update cascade;
+
 create sequence product_attribute_seq
     start with 1
     increment by 1
@@ -38,18 +71,11 @@ create table product_attribute(
   key varchar(255) not null,
   value varchar(1000) not null);
 
-create sequence shop_category_seq
-    start with 1
-    increment by 1
-    no minvalue
-    no maxvalue
-    cache 1;
+alter table product_attribute add constraint product_attribute_pk
+primary key (id);
 
-create table shop_category(
-  id bigint default nextval('shop_category_seq'),
-  offer_id bigint not null,
-  original_id varchar(32) not null,
-  name varchar(255) not null,
-  parent_id bigint);
+alter table product_attribute add constraint product_attribute_product_fk
+foreign key (product_id) references product(id)
+on update cascade on delete cascade;
 
 end;
