1f4a61cb8b1d5645ed20a003e265dfbc397f8a32
filipovskii_off <filipovskii.off@gmail.com>
2012-10-19 13:33:43 +0400

    Transaction in user-secret-key migration.

--- src/sql/SNAPSHOT/0183-user-secret-key.sql
+++ src/sql/SNAPSHOT/0183-user-secret-key.sql
@@ -1,3 +1,4 @@
+begin;
 alter table user_profile
 add column secret_key varchar(32) not null default md5(random() :: text);
 
@@ -5,3 +6,4 @@ alter table user_profile
 add constraint secret_key_unique unique (secret_key);
 
 create unique index secret_key_idx on user_profile (secret_key);
+end;
