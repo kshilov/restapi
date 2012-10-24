d0b3b796be43ebabe249f6138d8a53017bfd94a3
filipovskii_off <filipovskii.off@gmail.com>
2012-10-24 19:07:26 +0400

    Incorrect index syntax fixed.

--- src/sql/SNAPSHOT/0183-6-indexes.sql
+++ src/sql/SNAPSHOT/0183-6-indexes.sql
@@ -1,8 +1,8 @@
 begin
-create index if not exists product_offer_id_idx
+create index product_offer_id_idx
 on product(offer_id);
 
-create index if not exists product_attribute_product_id_idx 
+create index product_attribute_product_id_idx 
 on product_attribute(product_id);
 end
 
