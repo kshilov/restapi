96c37af8ab54d81ac6cafc6331339d2c0bc7661c
filipovskii_off <filipovskii.off@gmail.com>
2012-10-25 13:57:26 +0400

    Migration for product suboffers.

--- src/sql/SNAPSHOT/0183-4-is-product-offer-field.sql
+++ src/sql/SNAPSHOT/0183-4-is-product-offer-field.sql
@@ -3,6 +3,6 @@ alter table offer
 add column is_product_offer boolean not null default false;
 
 update offer
-set is_product_offer = true where exclusive = true;
+set is_product_offer = true where exclusive = true and parent_id = null;
 
 end;
