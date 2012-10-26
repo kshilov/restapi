36cfa0b94b4a9bf649755b2c07ff93768376ddc5
filipovskii_off <filipovskii.off@gmail.com>
2012-10-26 18:04:58 +0400

    ProductCategoryMapping productId index.

--- src/sql/SNAPSHOT/0183-6-indexes.sql
+++ src/sql/SNAPSHOT/0183-6-indexes.sql
@@ -6,3 +6,6 @@ on product_attribute(product_id);
 
 create index shop_category_offer_id_idx
 on shop_category(offer_id);
+
+create index product_category_product_id_idx
+on product_category(product_id);
