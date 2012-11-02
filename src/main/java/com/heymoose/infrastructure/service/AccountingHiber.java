08edb02919338cdb2603787a3aa1bc4996220fcd
filipovskii_off <filipovskii.off@gmail.com>
2012-11-02 18:19:31 +0400

    Sign check.

--- src/main/java/com/heymoose/infrastructure/service/AccountingHiber.java
+++ src/main/java/com/heymoose/infrastructure/service/AccountingHiber.java
@@ -182,7 +182,7 @@ public class AccountingHiber implements Accounting {
         .amount(affiliatePart)
         .event(AccountingEvent.ACTION_CREATED)
         .sourceId(action.id()).execute();
-    if (heymoosePart.equals(BigDecimal.ZERO)) return;
+    if (heymoosePart.signum() == 0) return;
     this.newTransfer()
         .from(action.offer().account())
         .to(adminAccounts.getAdminAccountNotConfirmed())
