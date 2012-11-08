0b4d682f5ccad030eb5ef94861ed7217356d6019
filipovskii_off <filipovskii.off@gmail.com>
2012-11-08 17:16:03 +0400

    ReportAction is longer then 10..

--- src/sql/SNAPSHOT/0203-lead-stat.sql
+++ src/sql/SNAPSHOT/0203-lead-stat.sql
@@ -15,7 +15,7 @@ create table lead_stat(
   token_id bigint not null,
   referrer varchar(255),
   ip varchar(15),
-  method varchar(10),
+  method varchar(15),
   creation_time timestamp without time zone default now() not null);
 
 alter table lead_stat
