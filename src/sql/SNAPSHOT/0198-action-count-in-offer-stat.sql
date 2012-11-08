1759774df004af7e2ddaf821f2f098f521c83767
filipovskii_off <filipovskii.off@gmail.com>
2012-11-08 18:40:04 +0400

    Migration for existing data.

--- src/sql/SNAPSHOT/0198-action-count-in-offer-stat.sql
+++ src/sql/SNAPSHOT/0198-action-count-in-offer-stat.sql
@@ -10,3 +10,23 @@ alter table offer_stat
 add column canceled_action_count int not null default 0;
 
 end;
+
+begin;
+
+update offer_stat set action_count = leads_count + sales_count;
+
+update offer_stat
+set canceled_action_count = (select count(*)
+                             from offer_action
+                             where stat_id = offer_stat.id
+                             and state = 2)
+where coalesce(canceled_revenue, 0.0) > 0;
+
+update offer_stat
+set confirmed_action_count = (select count(*)
+                             from offer_action
+                             where stat_id = offer_stat.id
+                             and state = 1)
+where coalesce(confirmed_revenue, 0.0) > 0;
+
+end;
