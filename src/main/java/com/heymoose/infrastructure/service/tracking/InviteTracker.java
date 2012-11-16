741c135d74532e97666f747b0a97aecf99c7b94b
filipovskii_off <filipovskii.off@gmail.com>
2012-11-16 17:09:51 +0400

    Logging in cashback invite tracker.

--- src/main/java/com/heymoose/infrastructure/service/tracking/InviteTracker.java
+++ src/main/java/com/heymoose/infrastructure/service/tracking/InviteTracker.java
@@ -4,14 +4,18 @@ import com.google.common.base.Strings;
 import com.heymoose.resource.api.ApiExceptions;
 import com.heymoose.resource.api.ApiRequestException;
 import com.sun.jersey.api.core.HttpRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.Response;
 import java.util.Map;
 
-import static com.heymoose.infrastructure.service.tracking.TrackingUtils.addCookie;
-import static com.heymoose.infrastructure.service.tracking.TrackingUtils.queryParams;
+import static com.heymoose.infrastructure.service.tracking.TrackingUtils.*;
 
 public class InviteTracker implements Tracker {
+
+  private static final Logger log =
+      LoggerFactory.getLogger(InviteTracker.class);
 
   public static final String COOKIE_NAME = "hm_cashback_invite";
   public static final String REFERRER_PARAM = "referrer";
@@ -21,16 +25,17 @@ public class InviteTracker implements Tracker {
                     Response.ResponseBuilder response)
       throws ApiRequestException {
     Map<String, String> queryParams = queryParams(context);
-    String referer = queryParams.get(REFERRER_PARAM);
+    String referrer = queryParams.get(REFERRER_PARAM);
     String ulp = queryParams.get(LOCATION_PARAM);
 
-    if (Strings.isNullOrEmpty(referer))
+    if (Strings.isNullOrEmpty(referrer))
       throw ApiExceptions.nullParam(REFERRER_PARAM);
     if (Strings.isNullOrEmpty(ulp))
       throw ApiExceptions.nullParam(LOCATION_PARAM);
 
-    addCookie(response, COOKIE_NAME, referer, Integer.MAX_VALUE);
+    addCookie(response, COOKIE_NAME, referrer, Integer.MAX_VALUE);
     if (!ulp.contains("://")) ulp = "http://" + ulp;
+    log.info("Tracking cashback invite. referrer: {}, ulp: {}", referrer, ulp);
     response.status(302).header("Location", ulp);
   }
 }
