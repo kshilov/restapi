895e872806dff9a1ccf2a1189f8c49478a7b5aff
filipovskii_off <filipovskii.off@gmail.com>
2012-11-27 14:41:30 +0400

    Migration fixes.

--- src/sql/SNAPSHOT/0205-2-site-in-stat-and-action.sql
+++ src/sql/SNAPSHOT/0205-2-site-in-stat-and-action.sql
@@ -2,7 +2,7 @@ alter table offer_stat
 add column site_id bigint;
 
 alter table offer_stat
-add constraint offer_site_site_fk foreign key (site_id) references site(id);
+add constraint offer_stat_site_fk foreign key (site_id) references site(id);
 
 alter table offer_action
 add column site_id bigint;
